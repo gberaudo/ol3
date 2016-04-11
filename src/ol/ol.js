fe65e41efacc8864f607c6749cd4d8ef652460a3
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2016-04-11 23:49:05 +0200

    Migrate ol namespace as a goog.module

--- src/ol/ol.js
+++ src/ol/ol.js
@@ -1,4 +1,5 @@
-goog.provide('ol');
+goog.module('ol');
+goog.module.declareLegacyNamespace();
 
 
 /**
@@ -11,7 +12,7 @@ goog.provide('ol');
 /**
  * @define {boolean} Assume touch.  Default is `false`.
  */
-ol.ASSUME_TOUCH = false;
+exports.ASSUME_TOUCH = false;
 
 
 /**
@@ -19,38 +20,38 @@ ol.ASSUME_TOUCH = false;
  * see https://github.com/openlayers/ol3/issues/2076
  * @define {number} Default maximum zoom for default tile grids.
  */
-ol.DEFAULT_MAX_ZOOM = 42;
+exports.DEFAULT_MAX_ZOOM = 42;
 
 
 /**
  * @define {number} Default min zoom level for the map view.  Default is `0`.
  */
-ol.DEFAULT_MIN_ZOOM = 0;
+exports.DEFAULT_MIN_ZOOM = 0;
 
 
 /**
  * @define {number} Default maximum allowed threshold  (in pixels) for
  *     reprojection triangulation. Default is `0.5`.
  */
-ol.DEFAULT_RASTER_REPROJECTION_ERROR_THRESHOLD = 0.5;
+exports.DEFAULT_RASTER_REPROJECTION_ERROR_THRESHOLD = 0.5;
 
 
 /**
  * @define {number} Default tile size.
  */
-ol.DEFAULT_TILE_SIZE = 256;
+exports.DEFAULT_TILE_SIZE = 256;
 
 
 /**
  * @define {string} Default WMS version.
  */
-ol.DEFAULT_WMS_VERSION = '1.3.0';
+exports.DEFAULT_WMS_VERSION = '1.3.0';
 
 
 /**
  * @define {number} Hysteresis pixels.
  */
-ol.DRAG_BOX_HYSTERESIS_PIXELS = 8;
+exports.DRAG_BOX_HYSTERESIS_PIXELS = 8;
 
 
 /**
@@ -58,7 +59,7 @@ ol.DRAG_BOX_HYSTERESIS_PIXELS = 8;
  *     this to false at compile time in advanced mode removes all code
  *     supporting the Canvas renderer from the build.
  */
-ol.ENABLE_CANVAS = true;
+exports.ENABLE_CANVAS = true;
 
 
 /**
@@ -67,15 +68,15 @@ ol.ENABLE_CANVAS = true;
  *     in advanced mode removes all code supporting the DOM renderer from the
  *     build.
  */
-ol.ENABLE_DOM = true;
+exports.ENABLE_DOM = true;
 
 
 /**
- * @define {boolean} Enable rendering of ol.layer.Image based layers.  Default
+ * @define {boolean} Enable rendering of exports.layer.Image based layers.  Default
  *     is `true`. Setting this to false at compile time in advanced mode removes
  *     all code supporting Image layers from the build.
  */
-ol.ENABLE_IMAGE = true;
+exports.ENABLE_IMAGE = true;
 
 
 /**
@@ -85,45 +86,45 @@ ol.ENABLE_IMAGE = true;
  *     with Canvas renderer, which uses its own names, whether this is true or
  *     false.
  */
-ol.ENABLE_NAMED_COLORS = false;
+exports.ENABLE_NAMED_COLORS = false;
 
 
 /**
  * @define {boolean} Enable integration with the Proj4js library.  Default is
  *     `true`.
  */
-ol.ENABLE_PROJ4JS = true;
+exports.ENABLE_PROJ4JS = true;
 
 
 /**
  * @define {boolean} Enable automatic reprojection of raster sources. Default is
  *     `true`.
  */
-ol.ENABLE_RASTER_REPROJECTION = true;
+exports.ENABLE_RASTER_REPROJECTION = true;
 
 
 /**
- * @define {boolean} Enable rendering of ol.layer.Tile based layers.  Default is
+ * @define {boolean} Enable rendering of exports.layer.Tile based layers.  Default is
  *     `true`. Setting this to false at compile time in advanced mode removes
  *     all code supporting Tile layers from the build.
  */
-ol.ENABLE_TILE = true;
+exports.ENABLE_TILE = true;
 
 
 /**
- * @define {boolean} Enable rendering of ol.layer.Vector based layers.  Default
+ * @define {boolean} Enable rendering of exports.layer.Vector based layers.  Default
  *     is `true`. Setting this to false at compile time in advanced mode removes
  *     all code supporting Vector layers from the build.
  */
-ol.ENABLE_VECTOR = true;
+exports.ENABLE_VECTOR = true;
 
 
 /**
- * @define {boolean} Enable rendering of ol.layer.VectorTile based layers.
+ * @define {boolean} Enable rendering of exports.layer.VectorTile based layers.
  *     Default is `true`. Setting this to false at compile time in advanced mode
  *     removes all code supporting VectorTile layers from the build.
  */
-ol.ENABLE_VECTOR_TILE = true;
+exports.ENABLE_VECTOR_TILE = true;
 
 
 /**
@@ -131,48 +132,48 @@ ol.ENABLE_VECTOR_TILE = true;
  *     this to false at compile time in advanced mode removes all code
  *     supporting the WebGL renderer from the build.
  */
-ol.ENABLE_WEBGL = true;
+exports.ENABLE_WEBGL = true;
 
 
 /**
  * @define {number} The size in pixels of the first atlas image. Default is
  * `256`.
  */
-ol.INITIAL_ATLAS_SIZE = 256;
+exports.INITIAL_ATLAS_SIZE = 256;
 
 
 /**
  * @define {number} The maximum size in pixels of atlas images. Default is
- * `-1`, meaning it is not used (and `ol.WEBGL_MAX_TEXTURE_SIZE` is
+ * `-1`, meaning it is not used (and `exports.WEBGL_MAX_TEXTURE_SIZE` is
  * used instead).
  */
-ol.MAX_ATLAS_SIZE = -1;
+exports.MAX_ATLAS_SIZE = -1;
 
 
 /**
  * @define {number} Maximum mouse wheel delta.
  */
-ol.MOUSEWHEELZOOM_MAXDELTA = 1;
+exports.MOUSEWHEELZOOM_MAXDELTA = 1;
 
 
 /**
  * @define {number} Mouse wheel timeout duration.
  */
-ol.MOUSEWHEELZOOM_TIMEOUT_DURATION = 80;
+exports.MOUSEWHEELZOOM_TIMEOUT_DURATION = 80;
 
 
 /**
  * @define {number} Maximum width and/or height extent ratio that determines
  * when the overview map should be zoomed out.
  */
-ol.OVERVIEWMAP_MAX_RATIO = 0.75;
+exports.OVERVIEWMAP_MAX_RATIO = 0.75;
 
 
 /**
  * @define {number} Minimum width and/or height extent ratio that determines
  * when the overview map should be zoomed in.
  */
-ol.OVERVIEWMAP_MIN_RATIO = 0.1;
+exports.OVERVIEWMAP_MIN_RATIO = 0.1;
 
 
 /**
@@ -183,9 +184,9 @@ ol.OVERVIEWMAP_MIN_RATIO = 0.1;
  *     This can happen if the developer defines projections improperly and/or
  *     with unlimited extents.
  *     If too many tiles are required, no tiles are loaded and
- *     `ol.TileState.ERROR` state is set. Default is `100`.
+ *     `exports.TileState.ERROR` state is set. Default is `100`.
  */
-ol.RASTER_REPROJECTION_MAX_SOURCE_TILES = 100;
+exports.RASTER_REPROJECTION_MAX_SOURCE_TILES = 100;
 
 
 /**
@@ -195,7 +196,7 @@ ol.RASTER_REPROJECTION_MAX_SOURCE_TILES = 100;
  *     At most `2*(2^this)` triangles are created for each triangulated
  *     extent (tile/image). Default is `10`.
  */
-ol.RASTER_REPROJECTION_MAX_SUBDIVISION = 10;
+exports.RASTER_REPROJECTION_MAX_SUBDIVISION = 10;
 
 
 /**
@@ -204,22 +205,22 @@ ol.RASTER_REPROJECTION_MAX_SUBDIVISION = 10;
  *     the resulting triangulation seems to have zero error and no subdivision
  *     is performed.
  *     If the triangle width is more than this (relative to world width; 0-1),
- *     subdivison is forced (up to `ol.RASTER_REPROJECTION_MAX_SUBDIVISION`).
+ *     subdivison is forced (up to `exports.RASTER_REPROJECTION_MAX_SUBDIVISION`).
  *     Default is `0.25`.
  */
-ol.RASTER_REPROJECTION_MAX_TRIANGLE_WIDTH = 0.25;
+exports.RASTER_REPROJECTION_MAX_TRIANGLE_WIDTH = 0.25;
 
 
 /**
  * @define {number} Tolerance for geometry simplification in device pixels.
  */
-ol.SIMPLIFY_TOLERANCE = 0.5;
+exports.SIMPLIFY_TOLERANCE = 0.5;
 
 
 /**
  * @define {number} Texture cache high water mark.
  */
-ol.WEBGL_TEXTURE_CACHE_HIGH_WATER_MARK = 1024;
+exports.WEBGL_TEXTURE_CACHE_HIGH_WATER_MARK = 1024;
 
 
 /**
@@ -228,7 +229,7 @@ ol.WEBGL_TEXTURE_CACHE_HIGH_WATER_MARK = 1024;
  * @const
  * @type {number|undefined}
  */
-ol.WEBGL_MAX_TEXTURE_SIZE; // value is set in `ol.has`
+exports.WEBGL_MAX_TEXTURE_SIZE; // value is set in `exports.has`
 
 
 /**
@@ -236,7 +237,7 @@ ol.WEBGL_MAX_TEXTURE_SIZE; // value is set in `ol.has`
  * @const
  * @type {Array.<string>}
  */
-ol.WEBGL_EXTENSIONS; // value is set in `ol.has`
+exports.WEBGL_EXTENSIONS; // value is set in `exports.has`
 
 
 /**
@@ -251,7 +252,7 @@ ol.WEBGL_EXTENSIONS; // value is set in `ol.has`
  *       // Call parent constructor
  *       ParentClass.call(this, a, b);
  *     }
- *     ol.inherits(ChildClass, ParentClass);
+ *     exports.inherits(ChildClass, ParentClass);
  *
  *     var child = new ChildClass('a', 'b', 'see');
  *     child.foo(); // This works.
@@ -261,7 +262,7 @@ ol.WEBGL_EXTENSIONS; // value is set in `ol.has`
  * @function
  * @api
  */
-ol.inherits = function(childCtor, parentCtor) {
+exports.inherits = function(childCtor, parentCtor) {
   childCtor.prototype = Object.create(parentCtor.prototype);
   childCtor.prototype.constructor = childCtor;
 };
@@ -272,7 +273,7 @@ ol.inherits = function(childCtor, parentCtor) {
  *
  * @return {undefined} Nothing.
  */
-ol.nullFunction = function() {};
+exports.nullFunction = function() {};
 
 
-ol.global = Function('return this')();
+exports.global = Function('return this')();

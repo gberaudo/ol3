ea80fda55d99b1767c8178807a1a1d431bb6d5b1
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2016-04-20 15:18:00 +0200

    Move source and image source typedefs to implicit typedefs.js
    Require closure-util update.

---
+++ src/ol/typedefs.js
@@ -0,0 +1,21 @@
+/**
+ * @typedef {{attributions: (ol.AttributionLike|undefined),
+ *            logo: (string|olx.LogoOptions|undefined),
+ *            projection: ol.proj.ProjectionLike,
+ *            state: (ol.source.State|undefined),
+ *            wrapX: (boolean|undefined)}}
+ */
+ol.source.SourceOptions;
+
+
+/**
+ * @typedef {{attributions: (ol.AttributionLike|undefined),
+ *            extent: (null|ol.Extent|undefined),
+ *            logo: (string|olx.LogoOptions|undefined),
+ *            projection: ol.proj.ProjectionLike,
+ *            resolutions: (Array.<number>|undefined),
+ *            state: (ol.source.State|undefined)}}
+ */
+ol.source.ImageOptions;
+
+

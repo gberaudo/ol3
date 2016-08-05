c3e1b15bbb8760c57f53b926481b20131a6f5f5c
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2016-08-05 10:55:29 +0200

    Remove lines from doclet test results / depend on perl
    The code depends on perl for multiline search/replace.
    Line numbers may be different when switching to goog.modules.

--- test_doclets/run.sh
+++ test_doclets/run.sh
@@ -3,6 +3,9 @@ do
   node_modules/.bin/jsdoc -c config/jsdoc/info/conf.json $d/module -X > $d/module/result_x
   node_modules/.bin/jsdoc -c config/jsdoc/info/conf.json $d/provide -X > $d/provide/result_x
 
+  perl -0777 -i.with_lines -0pe 's/"range"[^\]]+\]/"range": []/igs' $d/module/result_x
+  sed -i 's/"lineno".*,/"lineno": 0,/' $d/module/result_x
+
   node_modules/.bin/jsdoc -c config/jsdoc/info/conf.json $d/module | sed 's/^.*"path": .*$//g' > $d/module/result
   node_modules/.bin/jsdoc -c config/jsdoc/info/conf.json $d/provide | sed 's/^.*"path": .*$//g' > $d/provide/result
 done

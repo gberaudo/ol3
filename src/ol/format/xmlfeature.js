37f1fd9477c72ea39b6aacf61d7e46eaf508208b
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2017-01-13 23:39:39 +0100

    Handle concrete classes with non implemented abstract methods
    TODO: do not disable eslint valid-jsdoc globally (see package.json)

--- src/ol/format/xmlfeature.js
+++ src/ol/format/xmlfeature.js
@@ -73,12 +73,13 @@ ol.format.XMLFeature.prototype.readFeatureFromDocument = function(
 
 
 /**
- * @abstract
  * @param {Node} node Node.
  * @param {olx.format.ReadOptions=} opt_options Options.
  * @return {ol.Feature} Feature.
  */
-ol.format.XMLFeature.prototype.readFeatureFromNode = function(node, opt_options) {};
+ol.format.XMLFeature.prototype.readFeatureFromNode = function(node, opt_options) {
+  throw new Error('Not implemented');
+};
 
 
 /**
@@ -148,23 +149,25 @@ ol.format.XMLFeature.prototype.readGeometry = function(source, opt_options) {
 
 
 /**
- * @abstract
  * @param {Document} doc Document.
  * @param {olx.format.ReadOptions=} opt_options Options.
  * @protected
  * @return {ol.geom.Geometry} Geometry.
  */
-ol.format.XMLFeature.prototype.readGeometryFromDocument = function(doc, opt_options) {};
+ol.format.XMLFeature.prototype.readGeometryFromDocument = function(doc, opt_options) {
+  throw new Error('Not implemented');
+};
 
 
 /**
- * @abstract
  * @param {Node} node Node.
  * @param {olx.format.ReadOptions=} opt_options Options.
  * @protected
  * @return {ol.geom.Geometry} Geometry.
  */
-ol.format.XMLFeature.prototype.readGeometryFromNode = function(node, opt_options) {};
+ol.format.XMLFeature.prototype.readGeometryFromNode = function(node, opt_options) {
+  throw new Error('Not implemented');
+};
 
 
 /**
@@ -214,13 +217,14 @@ ol.format.XMLFeature.prototype.writeFeature = function(feature, opt_options) {
 
 
 /**
- * @abstract
  * @param {ol.Feature} feature Feature.
  * @param {olx.format.WriteOptions=} opt_options Options.
  * @protected
  * @return {Node} Node.
  */
-ol.format.XMLFeature.prototype.writeFeatureNode = function(feature, opt_options) {};
+ol.format.XMLFeature.prototype.writeFeatureNode = function(feature, opt_options) {
+  throw new Error('Not implemented');
+};
 
 
 /**
@@ -233,12 +237,13 @@ ol.format.XMLFeature.prototype.writeFeatures = function(features, opt_options) {
 
 
 /**
- * @abstract
  * @param {Array.<ol.Feature>} features Features.
  * @param {olx.format.WriteOptions=} opt_options Options.
  * @return {Node} Node.
  */
-ol.format.XMLFeature.prototype.writeFeaturesNode = function(features, opt_options) {};
+ol.format.XMLFeature.prototype.writeFeaturesNode = function(features, opt_options) {
+  throw new Error('Not implemented');
+};
 
 
 /**
@@ -251,9 +256,10 @@ ol.format.XMLFeature.prototype.writeGeometry = function(geometry, opt_options) {
 
 
 /**
- * @abstract
  * @param {ol.geom.Geometry} geometry Geometry.
  * @param {olx.format.WriteOptions=} opt_options Options.
  * @return {Node} Node.
  */
-ol.format.XMLFeature.prototype.writeGeometryNode = function(geometry, opt_options) {};
+ol.format.XMLFeature.prototype.writeGeometryNode = function(geometry, opt_options) {
+  throw new Error('Not implemented');
+};

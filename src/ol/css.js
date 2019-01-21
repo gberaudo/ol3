23f1169c6c033d1f4d3079364a96cdc799020b29
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2019-01-21 16:35:13 +0100

    FIXME: ignore font issues
    - font handling is only available in the main thread;
    - we will need configuration / extra communication or heurisitcs.

--- src/ol/css.js
+++ src/ol/css.js
@@ -62,12 +62,16 @@ export const CLASS_COLLAPSED = 'ol-collapsed';
  * Get the list of font families from a font spec.  Note that this doesn't work
  * for font families that have commas in them.
  * @param {string} The CSS font property.
- * @return {Object<string>} The font families (or null if the input spec is invalid).
+ * @return {Object<string>} The font families (or null if the input spec is invalid or we are in a worker).
  */
 export const getFontFamilies = (function() {
   let style;
   const cache = {};
   return function(font) {
+    if (typeof document === 'undefined') {
+      return null;
+    }
+
     if (!style) {
       style = document.createElement('div').style;
     }
